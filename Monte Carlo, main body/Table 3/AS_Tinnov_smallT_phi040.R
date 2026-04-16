d414992a7cd15eaa896631211b0edb71cedae58d
PierValla95 <65169003+PierValla95@users.noreply.github.com>
2026-04-16 09:47:35 +0200

    Update AS_Tinnov_smallT_phi040.R

--- Monte Carlo, main body/Table 3/AS_Tinnov_smallT_phi040.R
+++ Monte Carlo, main body/Table 3/AS_Tinnov_smallT_phi040.R
@@ -2,7 +2,7 @@ library(RandAlphaTest)
 library(parallel)
 
 # For parallel computing
-iH <- 10
+iH <- 10 # Number of cores
 set.seed(150)
 cluster = makeCluster(iH)
 vSeeds = sample(1:1e5, iH, replace = FALSE)
@@ -16,12 +16,12 @@ for (h in 1:iH) {
 
 iM = 1000 # Number of MC samples
 vN = c(100, 200, 500) #Cross-sectional sizes
-vT = c(100, 200, 300, 500) # Temporal sizes
+vT = c(100, 200, 300, 400, 500) # Temporal sizes
 
 
 # Repository, change according to your PC
-sPathData = "C:/Users/pierl/Dropbox/Massacci_Sarno_Trapani2/Simulations/sim_data/Power/"
-sType = "power"
+sPathData = "C:/Users/pierl/Dropbox/Massacci_Sarno_Trapani2/Simulations/sim_data/Size/"
+sType = "Size"
 
 
 dC = qcauchy(0.95) # Critical value
@@ -43,7 +43,6 @@ for(n  in 1:length(vN)){
     }, lFoo=lFoo, iT = iT)
     # Store rejection frequencies
     mRej[n,j] = mean(vTstat > dC)
-    print(c(iN, iT, mRej[n,j]))
   }
 }
 
