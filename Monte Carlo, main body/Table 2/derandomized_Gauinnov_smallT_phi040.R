2b616d1ee799d537d56d4131de0fa1c2ae3ea273
PierValla95 <65169003+PierValla95@users.noreply.github.com>
2026-04-16 08:45:10 +0200

    Add files via upload

--- Monte Carlo, main body/Table 2/derandomized_Gauinnov_smallT_phi040.R
+++ Monte Carlo, main body/Table 2/derandomized_Gauinnov_smallT_phi040.R
@@ -2,7 +2,7 @@ library(RandAlphaTest)
 library(parallel)
 
 # For parallel computing
-iH <- 15 #Number of cores
+iH <- 15
 set.seed(150)
 cluster = makeCluster(iH)
 vSeeds = sample(1:1e5, iH, replace = FALSE)
@@ -16,34 +16,32 @@ for (h in 1:iH) {
 iM = 1000 # Number of MC samples
 
 
-vN = c(100, 200, 500) # Cross-sectional sizes
-vT = c(100, 200, 300, 500) # Temporal sizes
+vN = c(100, 200, 500)
+vT = c(100, 200, 300, 500)
 
-# Load simulated data. Change according to your working directory
 sPathData = "C:/Users/pierl/Dropbox/Massacci_Sarno_Trapani2/Simulations/sim_data/Power/"
+
+dAlpha = 0.05
+
+dNu <- 5
+dQ = 1/4
+
 sType = "power"
+mRes1 = matrix(0.0, ncol = length(vT), nrow = length(vN))
+mRes2 = matrix(0.0, ncol = length(vT), nrow = length(vN))
 load(paste(sPathData, sType, "LatentFactor_PhiNu040.Rdata", sep = ""))
-
-dAlpha = 0.05 # Level of the one-shot tests
-dNu <- 5 # Power for psi
-dQ = 1/4 # Power for f(B) in the modified threshold
-
-# Pre-allocation matrices
-mRes1 = matrix(0.0, ncol = length(vT), nrow = length(vN)) # For LIL-based threshold 
-mRes2 = matrix(0.0, ncol = length(vT), nrow = length(vN)) # For f(B) based threshold
-
 for(n  in 1:length(vN)){
   iN = vN[n]
-  iB = (floor(log(iN)^2)) # Number of trials in the de-randomized procedure
+  iB = (floor(log(iN)^2))
 
   mZ = matrix(0.0, nrow = iM, ncol =iB)
 
-  lAB = get_AB(iN) # normalizing sequences for EVT asymptotics
-  dC = get_criticalValues_ultimate(lAB$A, lAB$B, dAlpha) # Gumbel-based critical value
+  lAB = get_AB(iN)
+  dC = get_criticalValues_ultimate(lAB$A, lAB$B, dAlpha)
   lFoo = lData[[n]]
 
-  dCritic1 = (1.0-dAlpha) - sqrt(dAlpha*(1.0-dAlpha))*sqrt(2.0*log(log(iB))/iB) # LIL-based threshold
-  dCritic2 = (1.0-dAlpha) - iB^(-dQ)  # f(B) based threshold
+  dCritic1 = (1.0-dAlpha) - sqrt(dAlpha*(1.0-dAlpha))*sqrt(2.0*log(log(iB))/iB)
+  dCritic2 = (1.0-dAlpha) - iB^(-dQ)
 
   for(j in 1:length(vT)){
     iT = vT[j]
@@ -51,23 +49,18 @@ for(n  in 1:length(vN)){
 
     for(m in 1:iM){
       set.seed(m)
-      
       lFoo1 = list(Y = lFoo[[m]]$Y[, 1:iT], X = lFoo[[m]]$X[, 1:iT])
       clusterExport(cl=cluster, c("dNu", "dTeff"))
-
-      # Test statistics for the B trials over the m-th Monte Carlo sample
       mZ[m, ] = parSapply(cl = cluster, 1:iB, function(b, lFoo1){
         set.seed(b)
         return(get_testStat.fast(lFoo1$Y, lFoo1$X, dNu, dTeff))
       }, lFoo1 = lFoo1)
     }
 
-    # Create a vector of Qs across Monte Carlo samples
     vQ = sapply(1:iM, function(m){mean(mZ[m, ]<=dC)})
-
-    # Store rejection frequencies for both thresholds
     mRes1[n, j] = mean(vQ < dCritic1)
     mRes2[n, j] = mean(vQ < dCritic2)
+    print(c(iT, iN, mRes1[n, j], mRes2[n, j]))
   }
 }
 
