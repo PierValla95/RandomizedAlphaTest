92227047aded1e6c076bfaace1d25165ab8cd8ef
PierValla95 <65169003+PierValla95@users.noreply.github.com>
2026-04-16 11:20:55 +0200

    Update FLLM_GARCHinnov_largeT_phi040.R

--- Monte Carlo, main body/Table 5/FLLM_GARCHinnov_largeT_phi040.R
+++ Monte Carlo, main body/Table 5/FLLM_GARCHinnov_largeT_phi040.R
@@ -2,7 +2,7 @@ library(RandAlphaTest)
 library(parallel)
 
 # For parallel computing
-iH <-5
+iH <-5 # Number of cores
 set.seed(150)
 cluster = makeCluster(iH)
 vSeeds = sample(1:1e5, iH, replace = FALSE)
@@ -17,7 +17,7 @@ for (h in 1:iH) {
 iK = 3 # Number of factors
 iM = 1000 # Number of MC samples
 
-# Parameters for Factors DGP and related GARCH models
+# Parameters for Factors DGP 
 vIntF = c(0.53, 0.19, 0.19)
 mPhi = diag(c(-0.1, 0.2, -0.2))
 
@@ -25,14 +25,17 @@ vN = c(100, 200, 500) # Cross-sectional sizes
 vT = c(1000, 2000) # Temporal sizes
 iT.max = max(vT)
 
+# Calculate critical values
 dAlpha = 0.05
-
-
 dC = get_criticalValuesFLLM(dAlpha)
+
+# Pre-allocation matrix for rejection frequencies
 mRej = matrix(0.0, ncol = length(vT), nrow = length(vN))
 
 for(n in seq_along(vN)){
   iN = vN[n]
+
+  # Simulate data. Use Type = "size" for under the null and Type = "power" for under the alternative
   lInput = list(iK = iK, iN = iN, iT = iT.max, Phi = mPhi, Type="size",
                 PhiNu = 0.40, vIntF = vIntF, dPerc = 0.95)
 
@@ -51,7 +54,6 @@ for(n in seq_along(vN)){
     # Store rejection frequencies
     mRej[n,j] = mean(vZ  - 2.0*log(iN) + log(log(iN)) > dC)
   }
-  print(mRej)
 }
 
 
