b192beebc68fa2552948fa5af0b83cb9e08f695c
PierValla95 <65169003+PierValla95@users.noreply.github.com>
2026-04-16 08:20:58 +0200

    Update PY_GauInnov_largeT_phi040.R

--- Monte Carlo, main body/Table 1/PY_GauInnov_largeT_phi040.R
+++ Monte Carlo, main body/Table 1/PY_GauInnov_largeT_phi040.R
@@ -2,7 +2,7 @@ library(RandAlphaTest)
 library(parallel)
 
 # For parallel computing
-iH <-5
+iH <-5 # Number of cores
 set.seed(150)
 cluster = makeCluster(iH)
 vSeeds = sample(1:1e5, iH, replace = FALSE)
@@ -17,7 +17,7 @@ for (h in 1:iH) {
 iK = 3 # Number of factors
 iM = 1000 # Number of MC samples
 
-# Parameters for Factors DGP and related GARCH models
+# Parameters for Factors DGP
 vIntF = c(0.53, 0.19, 0.19)
 mPhi = diag(c(-0.1, 0.2, -0.2))
 
@@ -25,34 +25,34 @@ vN = c(100, 200, 500) # Cross-sectional sizes
 vT = c(1000, 2000) # Temporal sizes
 iT.max = max(vT)
 
-dAlpha = 0.05
+dC = qnorm(0.95) # Critical value
 
-dC = qnorm(0.95)
+# Pre-allocation matrix for rejection frequencies
 mRej = matrix(0.0, ncol = length(vT), nrow = length(vN))
 
 for(n in seq_along(vN)){
   iN = vN[n]
+
+  # Simulate data. Set Type = "size" for under the null and Type = "power" for the alternative
   lInput = list(iK = iK, iN = iN, iT = iT.max, Phi = mPhi, Type="size",
                 PhiNu = 0.40, vIntF = vIntF, dPerc = 0.95)
 
   lData = get_data_latentFactor.bis(lInput)
   clusterExport(cl =cluster, "lData")
+  
   for(j in seq_along(vT)){
     iT = vT[j]
     clusterExport(cl =cluster, "iT")
 
     # Calculate test statistics
     vZ = parSapply(cl = cluster, seq(iM), function(m){
-      set.seed(m)
       return(get_testStatPY(lData[[m]]$Y[, 1:iT], lData[[m]]$X[, 1:iT]))
     })
 
     # Store rejection frequencies
     mRej[n,j] = mean(vZ > dC)
   }
-  print(mRej)
 }
-
 
 stopCluster(cluster)
 rm(list=ls())
