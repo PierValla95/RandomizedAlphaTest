9dd96045aecb06ebb39674c8cad1da99fe1abf0f
PierValla95 <65169003+PierValla95@users.noreply.github.com>
2026-04-16 08:17:57 +0200

    Update GRS_GauInnov_largeT_phi040.R

--- Monte Carlo, main body/Table 1/GRS_GauInnov_largeT_phi040.R
+++ Monte Carlo, main body/Table 1/GRS_GauInnov_largeT_phi040.R
@@ -3,7 +3,7 @@ library(RandAlphaTest)
 library(parallel)
 
 # For parallel computing
-iH <-5
+iH <-5 # Number of cores
 set.seed(150)
 cluster = makeCluster(iH)
 vSeeds = sample(1:1e5, iH, replace = FALSE)
@@ -18,7 +18,7 @@ for (h in 1:iH) {
 iK = 3 # Number of factors
 iM = 1000 # Number of MC samples
 
-# Parameters for Factors DGP and related GARCH models
+# Parameters for Factors DGP 
 vIntF = c(0.53, 0.19, 0.19)
 mPhi = diag(c(-0.1, 0.2, -0.2))
 
@@ -26,10 +26,7 @@ vN = c(100, 200, 500) # Cross-sectional sizes
 vT = c(1000, 2000) # Temporal sizes
 iT.max = max(vT)
 
-
-dAlpha = 0.05
-
-
+# Pre-allocation matrix for rejection frequencies
 mRej = matrix(0.0, ncol = length(vT), nrow = length(vN))
 for(n in seq_along(vN)){
   iN = vN[n]
@@ -44,7 +41,6 @@ for(n in seq_along(vN)){
 
     # Calculate test statistics
     vZ = parSapply(cl = cluster, seq(iM), function(m){
-      set.seed(m)
       return(get_testStatGRS(lData[[m]]$Y[, 1:iT], lData[[m]]$X[, 1:iT], iT, iN))
     })
 
@@ -54,7 +50,6 @@ for(n in seq_along(vN)){
     # Store rejection frequencies
     mRej[n,j] = mean(vZ > dC)
   }
-  print(mRej)
 }
 
 
